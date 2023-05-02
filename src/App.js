d2a76e3430045546f559843ae0a7638d084659c6
mahmoodhamza1261 <mahmoodhamza1261@gmail.com>
2023-05-02 22:26:20 +0500

    3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [Hori,setHori]=useState(10)
   const [Veri,setVeri]=useState(10)
   const [Blur,setBlur]=useState(10)
+  const [color,setColor]=useState("black")
+  const [checkOn,setcheckOn]=useState(false)
 
   return (
     <div className="App">
@@ -16,9 +18,21 @@ function App() {
     <input type="range"  min="-200" max="200" value={Veri} onChange={(e)=>setVeri(e.target.value)} />
     <label>blur</label>
     <input type="range"  min="0" max="200" value={Blur} onChange={(e)=>setBlur(e.target.value)} />
+    <label>color</label>
+    <input type="color"   value={color} onChange={(e)=>setColor(e.target.value)} />
     </div>
+    <div className="switch">
+    <label>
+      Outline
+      <input type="checkbox" checked={checkOn} onChange={()=>setcheckOn(!checkOn)}/>
+      <span className="lever"></span>
+      Inset
+    </label>
+  </div>
     <div className="output">
-      <div className="box"></div>
+      <div className="box" style={{boxShadow:`${checkOn?"inset":""} ${Hori}px ${Veri}px ${Blur}px ${color}`}}>
+        <p>box-shadow:{Hori}px {Veri}px {Blur}px {color}</p>
+      </div>
     </div>
     </div>
   );
